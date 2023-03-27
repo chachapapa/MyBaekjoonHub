3d2712ad982d960d8281b7492e312228801c3f33
chachapapa <119780428+chachapapa@users.noreply.github.com>
2023-03-27 12:22:52 +0900

    [Silver III] Title: 교수가 된 현우, Time: 916 ms, Memory: 33412 KB -BaekjoonHub

---
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int t = Integer.parseInt(br.readLine());
+
+		for (int test = 1; test <= t; test++) {
+			int n = Integer.parseInt(br.readLine());
+
+			
+			int count5 = 0;
+
+//			for (int i = n; i >= 1; i--) {
+//				int tmp = i;
+//
+//
+//				while (tmp % 5 == 0) {
+//					count5++;
+//					tmp/=5;
+//				}
+//			}
+			
+			while(n/5 != 0) {
+			count5+= n/5;
+			n /=5;
+			}
+			
+			
+			System.out.println(count5);
+		}
+	}
+}
