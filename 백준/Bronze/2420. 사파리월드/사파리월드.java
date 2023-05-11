6ebc0538b79f298fbf9bf357e7907b257f6af743
정재웅 <119780428+chachapapa@users.noreply.github.com>
2023-05-11 23:58:41 +0900

    [Bronze V] Title: 사파리월드, Time: 200 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+     
+		Scanner sc = new Scanner(System.in);
+		
+		long n = sc.nextLong();
+		long m = sc.nextLong();
+		
+		System.out.println(Math.abs(n-m));
+	}
+
+}
