a84c8d0de52167bb48b90d8c6edd9eeeec7c37a3
정재웅 <119780428+chachapapa@users.noreply.github.com>
2024-04-21 00:19:46 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.js
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.js
@@ -1,44 +1,55 @@
-const getInfo = (edges) => { 
-    const info = edges.reduce((map, key) => {
-    if (!map.has(key[0])) {
-        map.set(key[0], [1, 0]);
-    } else {
-        const [give, receive] = map.get(key[0]);
-        map.set(key[0], [give+1, receive]);
-    }
-    if(!map.has(key[1])){
-        map.set(key[1], [0, 1]);
-    } else {
-        const [give, receive] = map.get(key[1]);
-        map.set(key[1], [give, receive+1]);
-    }
-    return map;
-}, new Map());
-    return info;
-}
 
-const chkInfo = (info) => {
-    const res = new Array(4).fill(0); 
-    for(const [key, io] of info){ 
-        const [give, receive] = io;  
-        if( 2 <= give && receive == 0) { 
-            res[0] = key;
-        } else if(give == 0) {
-            //막대그래프 최상단은 give == 0
-            res[2]++;
-        } else if(give >= 2 && receive >= 2){
-            res[3]++;
-        }  
-    }       
-    // 도넛은 사이클이 이루어진다는 것 밖에 없기 때문에 개수 계산으로 판별할 수 없다. 
-    // 생성 정점은 기존에 존재하던 모든 그래프에 간선을 하나 씩 연결한다.
-    // 따라서, 생성 정점의 간선 개수에서 막대, 8자 그래프 개수를 빼면 도넛 그래프 개수가 나온다.
-    res[1] = info.get(res[0])[0] - res[2] - res[3];
-    return res;
-}
 
 const solution = (edges) => {
-    const mapInfo = getInfo(edges);
-    const answer = chkInfo(mapInfo);
+    //도넛 그래프 n 개의 정점 n개의 간선 => 순환그래프
+    //막대 그래프 직선 n개의 정점 n-1개의 간선 트리
+    //8자 그래프  2n+1개의 정점 2n+2개의 간선
+    //무관한 정점을 찾는게 포인트겠네
+    //들어오는 간선은 당연히 없을거구
+    //다른 그래프를 구성하는 노드가 들어오는 간선이 없는게 있나?
+    //있다. 막대모양그래프 첫째노드
+    //그럼 들어오는거 없는애들 다세서 -1 하면 막대그래프 갯수
+    // 고려하지 못한 예외가 있을 것 같다 일단..
+    // 찾았다. 각 그래프의 갯수 합은 2 이상.
+    
+    let inputCheck = new Map()
+    
+    for(let line of edges){
+        
+        if(!inputCheck.get(line[0])){
+            inputCheck.set(line[0],{in : 0, out : 1})
+        }else{
+            inputCheck.get(line[0]).out += 1
+        }
+        
+        if(!inputCheck.get(line[1])){
+            inputCheck.set(line[1],{in : 1, out : 0})
+        }else{
+            inputCheck.get(line[1]).in += 1
+        }
+    }
+    let myNode = 0
+    let total = 0
+    let doughnut = 0
+    let stick = 0
+    let eight = 0
+    for(let node of inputCheck){
+       
+        if(node[1].in === 0 && node[1].out >= 2){
+                myNode = node[0]
+                total = node[1].out
+        }
+        
+        if(node[1].out === 0){
+            stick += 1
+        }
+        
+        if(node[1].in >= 2 && node[1].out === 2){
+            eight += 1
+        }
+    }
+    
+    answer = [myNode,total-stick-eight, stick, eight]
+    
     return answer;
 }
