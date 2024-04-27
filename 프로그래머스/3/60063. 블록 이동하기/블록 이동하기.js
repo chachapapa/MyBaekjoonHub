4c8f6ffe475a3607af8748c46a3877c0e5936bf0
정재웅 <119780428+chachapapa@users.noreply.github.com>
2024-04-27 17:46:43 +0900

    [level 3] Title: 블록 이동하기, Time: 65.43 ms, Memory: 44 MB -BaekjoonHub

--- 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.js
+++ 프로그래머스/3/60063. 블록 이동하기/블록 이동하기.js
@@ -1,68 +1,143 @@
-function solution (board) {
-  const N = board.length;
-  const goal = N + '' + N;
-  const queue = [ [ [1,1], [1,2], 0 ] ];
-  const visit = new Set(["1112"]);
-  
-  const new_board = new Array(N+2).fill().map(_ => new Array(N+2).fill(1));
-  for(let i = 0; i < N; i++) {
-    for(let j = 0; j < N; j++) {
-      new_board[i+1][j+1] = board[i][j];
-    }
-  }
-  
-  while(queue.length) {
-    const [left, right, count] = queue.shift();
+let dx = [0,0,1,-1]
+let dy = [1,-1,0,0]
+let turn = [1,-1]
+let tx = []
+//축회전은 l, r 중 하나 잡고
+//나머지 하나를 기준에서 dx dy 더한값으로 변경하는 것.
+
+const posCheck = (left, right, board) => {
     
-    if(left.join('') === goal || right.join('') === goal) return count;
     
-    const nextPosition = getNextPosition(left, right, new_board);
-    for(const next of nextPosition) {
-      const [next_left, next_right] = next;
-      const key = next_left.join('') + next_right.join('');
-      if(!visit.has(key)) {
-        queue.push([next_left, next_right, count+1]);
-        visit.add(key);
-      }
+    if(left[0] < 0 ||left[0] >= board.length || left[1] < 0 || left[1] >= board.length){
+        return false
     }
-  }
+    
+    if(right[0] < 0 ||right[0] >= board.length || right[1] < 0 || right[1] >= board.length){
+        return false
+    }
+    
+    if(board[left[0]][left[1]] === 1 || board[right[0]][right[1]] === 1){
+        return false
+    }
+    
+    
+    return true
 }
 
-const getNextPosition = (left, right, board) => {
-  const result = [];
-  const X = 1, Y = 0;
-  const moves = [ [-1,0], [1, 0], [0,-1], [0,1] ];
-  
-  for(const move of moves) {
-    const [dy, dx] = move;
-    const next_left = [ left[Y]+dy, left[X]+dx ];
-    const next_right = [ right[Y]+dy, right[X]+dx ];
+const turnCheck = (now, board, direction, status) => {
     
-    if(board[next_left[Y]][next_left[X]] === 0 &&
-       board[next_right[Y]][next_right[X]] === 0) {
-      result.push([next_left, next_right]);
+    if(status === "horizontal"){
+        
+        if(now.left[0]+direction < 0 || now.left[0]+direction >= board.length) return false
+
+        if(board[now.left[0]+direction][now.left[1]] === 0 && board[now.right[0]+direction][now.right[1]] === 0){
+            return true
+        }else{
+            return false
+        }
+    }else{
+        
+        if(now.left[1]+direction < 0 || now.left[1]+direction >= board.length) return false
+        
+        if(board[now.left[0]][now.left[1]+direction] === 0 && board[now.right[0]][now.right[1]+direction] === 0){
+            return true
+        }else{
+            return false
+        }
     }
-  }
-  
-  const toward = [-1, 1];
-  
-  if(left[Y] === right[Y]) {
-    for(const dy of toward) {
-      if(board[left[Y]+dy][left[X]] === 0 &&
-         board[right[Y]+dy][right[X]] === 0) {
-        result.push([ left, [ left[Y]+dy, left[X] ] ]);
-        result.push([ [ right[Y]+dy, right[X] ], right ]);
-      }
+}
+
+const bfs = (board, sl, sr, visited) => {
+    let q = []
+    q.push({left : sl, right : sr, time : 0})
+    visited[sl[0]][sl[1]].push(sr.toString())
+    visited[sr[0]][sr[1]].push(sl.toString())
+    while(q.length > 0){
+        let now = q.shift()
+        if(now.left[0] === board.length-1 && now.left[1] === board.length-1){
+            return now.time
+        }
+        
+        if(now.right[0] === board.length-1 && now.right[1] === board.length-1){
+            return now.time
+        }
+        
+        for(let i = 0 ; i<4 ; i++){
+            //이동
+            let next = {left : [now.left[0]+dx[i],now.left[1]+dy[i]],
+                        right : [now.right[0]+dx[i],now.right[1]+dy[i]], 
+                        time : now.time+1}
+            if(posCheck(next.left,next.right, board)){
+                if(!visited[next.left[0]][next.left[1]].includes(next.right.toString())){
+                    q.push(next)
+                    visited[next.left[0]][next.left[1]].push(next.right.toString())
+                    visited[next.right[0]][next.right[1]].push(next.left.toString())
+                }
+            }
+        }
+        
+        let status = "vertical"
+        
+        if(now.left[0] === now.right[0]){
+            status = "horizontal"
+        }    
+        
+        
+        for(let direction of turn){
+            if(turnCheck(now, board, direction, status)){
+               let next1
+               let next2
+               
+               if(status === "horizontal"){
+                    next1 = {left : [now.left[0],now.left[1]],
+                        right : [now.left[0]+direction,now.left[1]], 
+                        time : now.time+1}
+                    next2 = {left : [now.right[0],now.right[1]],
+                        right : [now.right[0]+direction,now.right[1]], 
+                        time : now.time+1}
+                }else{
+                    next1 = {left : [now.left[0],now.left[1]],
+                        right : [now.left[0],now.left[1]+direction], 
+                        time : now.time+1}
+                    next2 = {left : [now.right[0],now.right[1]],
+                        right : [now.right[0],now.right[1]+direction], 
+                        time : now.time+1}
+                }
+               
+        
+               if(!visited[next1.left[0]][next1.left[1]].includes(next1.right.toString())){
+                    q.push(next1)
+                    visited[next1.left[0]][next1.left[1]].push(next1.right.toString())
+                    visited[next1.right[0]][next1.right[1]].push(next1.left.toString())
+                }
+            
+                if(!visited[next2.left[0]][next2.left[1]].includes(next2.right.toString())){
+                    q.push(next2)
+                    visited[next2.left[0]][next2.left[1]].push(next2.right.toString())
+                    visited[next2.right[0]][next2.right[1]].push(next2.left.toString())
+                }
+            }
+        }
+            
     }
-  } else {
-    for(const dx of toward) {
-      if(board[left[Y]][left[X]+dx] === 0 &&
-         board[right[Y]][right[X]+dx] === 0) {
-        result.push([ [left[Y], left[X]+dx ], left ]);
-        result.push([ right, [ right[Y], right[X]+dx ] ]);
-      }
-    }
-  }                  
-  
-  return result;
+    
+}
+
+
+function solution(board) {
+    var answer = 0;
+    //로봇은 두칸을 차지한다
+    //회전시 축의 대각선에 있는 칸이 막혀있으면 회전할 수 없다.
+    //n,n 까지 두칸중 하나만 들어가면 성공
+    //현재 방향에서 들어갈 수 없는 빈칸을 찾아야겠네..
+    //bfs긴 한데 조금 복잡한 bfs
+    //그냥 회전까지 경우의수로 넣어서 돌려야되나?
+    //이동이랑 같은 느낌으로?
+    //방문처리는 어떻게 처리하지.
+    //들어간 자리에 같이 들어간 좌표를 넣어야겠다.
+    
+    let visited = new Array(board.length).fill().map(_=>new Array(board.length).fill().map(()=>new Array()))
+    answer = bfs(board, [0,0], [0,1], visited)
+    
+    return answer;
 }
